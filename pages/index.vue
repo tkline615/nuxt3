6f8f0a72d5bc9d8cf99fd77f5d43000c89c6ddd1
Tom Kline <thomasgkline@yahoo.com>
2023-08-06 14:14:53 -0400

    added pages directory

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aut culpa maiores modi ad nihil beatae ut commodi labore deleniti.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aut culpa maiores modi ad nihil beatae ut commodi labore deleniti.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
