f2adc7460dea52b1d86deb5def677f6bf78ed078
Tom Kline <thomasgkline@yahoo.com>
2023-08-06 14:22:03 -0400

    added route parameters for product ids

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Products details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat provident obcaecati iusto quisquam sequi quasi hic eum, nostrum neque cumque?</p>
+    </div>
+</template>
+
+<script setup>
+    // this name must match the [id] value
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
