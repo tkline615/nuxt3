329095657bd228f8912d1a322f7b6ebd178f4b0c
Tom Kline <thomasgkline@yahoo.com>
2023-08-06 14:30:35 -0400

    added navigation via nuxtlink

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aut culpa maiores modi ad nihil beatae ut commodi labore deleniti.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aut culpa maiores modi ad nihil beatae ut commodi labore deleniti.</p>
