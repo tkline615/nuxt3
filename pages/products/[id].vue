c1393dee267d4a6c0ae5df174ead9885cadac4ad
Tom Kline <thomasgkline@yahoo.com>
2023-08-10 08:09:41 -0400

    added error pages and meta details

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <Head>
+            <Title>Nuxt Dojo | {{ product.title }}</Title>
+            <Meta name="description" :content="product.description"/>
+        </Head>
         <ProductDetails :product="product" />
     </div>
 </template>
@@ -11,6 +15,14 @@
 
     // fetch the product
     const { data: product } = await useFetch(uri, { key: id})
+
+    if (!product.value) {
+        throw createError({
+            statusCode: 404,
+            statusMessage: 'Product not found',
+            fatal: true
+        })
+    }
 
     definePageMeta({
         layout: 'products'
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -16,7 +16,13 @@
 
     // fetch the products 
     const { data: products } = await useFetch('https://fakestoreapi.com/products')
-        
+    
+    useHead({
+        title: 'Nuxt Dojo | Merch',
+        meta: [
+            { name: 'description', content: 'Nuxt 3 Merch'}
+        ]
+    })
 </script>
 
 <style scoped>
